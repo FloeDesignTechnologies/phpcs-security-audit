a8f899f4e9632da8db101a2c9ca4b41b1f89a8fb
Jonathan Marcil <jonathan.marcil@gmail.com>
2013-11-06 23:44:26 -0500

    optimization

--- Security/Sniffs/Drupal7/AdvisoriesContribSniff.php
+++ Security/Sniffs/Drupal7/AdvisoriesContribSniff.php
@@ -32,55 +32,56 @@ class Security_Sniffs_Drupal7_AdvisoriesContribSniff implements PHP_CodeSniffer_
 		if ($stackPtr > 0)
 			return;
 		$dversion = '7';
+        $fileName  = $phpcsFile->getFileName();
+		if (!preg_match('/\.info$/', $fileName))
+			return;
 		$utils = Security_Sniffs_UtilsFactory::getInstance('Drupal7');
 		$tokens = $phpcsFile->getTokens();
-        $fileName  = $phpcsFile->getFileName();
 
-		if (preg_match('/\.info$/', $fileName)) {
-			$info = $utils->drupal_parse_info_format(file_get_contents($fileName));
-			if (isset($info) && count($info) && array_key_exists('project', $info) && array_key_exists($info['project'], $utils::$ContribAdvisories)) {
-				if ($utils::$ContribAdvisories[$info['project']][0][0] == 'abandoned') {
-					$phpcsFile->addError("Module " . $info['project'] . " is abandoned due to a security issue the maintainer never fixed. Details: " . $utils::$ContribAdvisories[$info['project']][0][1], $stackPtr, 'D7ErrAdvisoriesContribAbandonned');
-					return;
-				}
-				if ($utils::$ContribAdvisories[$info['project']][0][0] == 'unsupported') {
-					$phpcsFile->addError("Module " . $info['project'] . " is unsupported due to unfixed security issue. The Drupal Security Team recommends that this module be uninstalled immediately Details: " . $utils::$ContribAdvisories[$info['project']][0][1], $stackPtr, 'D7ErrAdvisoriesContribUnsupported');
-					return;
-				}
-				if (array_key_exists('core', $info) && array_key_exists('version', $info)) {
-					if (strpos($info['core'], $dversion) === 0) {
-							foreach ($utils::$ContribAdvisories[$info['project']] as $vcve) {
-								list($a, $CVEversion) = explode('-', $vcve[0]);
-								if ($a != $info['core'])
-									echo "WARNING Drupal core version inconsistence!!";
-								list ($a, $mversion) = explode('-', $info['version']);
-								$CVEversion = (float) $CVEversion;
-								if (preg_match('/dev/', $vcve[0]))
-									$phpcsFile->addWarning("WARNING module " . $info['project'] . " does not have any release for the security fix, manual checking required. Details: " . $vcve[1], $stackPtr, 'D7WarnAdvisoriesContribDev');
-								if (preg_match('/rc|alpha|beta/', $vcve[0]))
-									$phpcsFile->addWarning("WARNING module " . $info['project'] . " is using special version tagging around the security fix, manual checking recommanded. Details: " . $vcve[1], $stackPtr, 'D7WarnAdvisoriesContribrc');
-								$mversion = (float) $mversion;
-								$diff = $CVEversion - $mversion;
-								if ($diff > 0 && $diff < 1)
-									$phpcsFile->addError("Module " . $info['project'] ." ". $info['version'] . " contains security issue and must be updated to at least $vcve[0]. Details: " . $vcve[1], $stackPtr, 'D7ErrAdvisoriesContribFoundMinor');
-								elseif ($diff >= 1)
-									$phpcsFile->addWarning("Module " . $info['project'] ." ". $info['version'] . " is out of date a major version and might contains security issue. " . $vcve[1], $stackPtr, 'D7WarnAdvisoriesContribFoundMajor');
-								elseif ($diff <= 0) {
-									if (preg_match('/x$/', $vcve[0])) {
-										$phpcsFile->addError("Module " . $info['project'] ." ". $info['version'] . " contains security issue to all $vcve[0] versions. " . $vcve[1], $stackPtr, 'D7ErrAdvisoriesContribFoundMajor');
-									} else {
-										//echo "$fileName: SAFE! " . $info['version'] . "\n";
-									}
+		$info = $utils->drupal_parse_info_format(file_get_contents($fileName));
+		if (isset($info) && count($info) && array_key_exists('project', $info) && array_key_exists($info['project'], $utils::$ContribAdvisories)) {
+			if ($utils::$ContribAdvisories[$info['project']][0][0] == 'abandoned') {
+				$phpcsFile->addError("Module " . $info['project'] . " is abandoned due to a security issue the maintainer never fixed. Details: " . $utils::$ContribAdvisories[$info['project']][0][1], $stackPtr, 'D7ErrAdvisoriesContribAbandonned');
+				return;
+			}
+			if ($utils::$ContribAdvisories[$info['project']][0][0] == 'unsupported') {
+				$phpcsFile->addError("Module " . $info['project'] . " is unsupported due to unfixed security issue. The Drupal Security Team recommends that this module be uninstalled immediately Details: " . $utils::$ContribAdvisories[$info['project']][0][1], $stackPtr, 'D7ErrAdvisoriesContribUnsupported');
+				return;
+			}
+			if (array_key_exists('core', $info) && array_key_exists('version', $info)) {
+				if (strpos($info['core'], $dversion) === 0) {
+						foreach ($utils::$ContribAdvisories[$info['project']] as $vcve) {
+							list($a, $CVEversion) = explode('-', $vcve[0]);
+							if ($a != $info['core'])
+								echo "WARNING Drupal core version inconsistence!!";
+							list ($a, $mversion) = explode('-', $info['version']);
+							$CVEversion = (float) $CVEversion;
+							if (preg_match('/dev/', $vcve[0]))
+								$phpcsFile->addWarning("WARNING module " . $info['project'] . " does not have any release for the security fix, manual checking required. Details: " . $vcve[1], $stackPtr, 'D7WarnAdvisoriesContribDev');
+							if (preg_match('/rc|alpha|beta/', $vcve[0]))
+								$phpcsFile->addWarning("WARNING module " . $info['project'] . " is using special version tagging around the security fix, manual checking recommanded. Details: " . $vcve[1], $stackPtr, 'D7WarnAdvisoriesContribrc');
+							$mversion = (float) $mversion;
+							$diff = $CVEversion - $mversion;
+							if ($diff > 0 && $diff < 1)
+								$phpcsFile->addError("Module " . $info['project'] ." ". $info['version'] . " contains security issue and must be updated to at least $vcve[0]. Details: " . $vcve[1], $stackPtr, 'D7ErrAdvisoriesContribFoundMinor');
+							elseif ($diff >= 1)
+								$phpcsFile->addWarning("Module " . $info['project'] ." ". $info['version'] . " is out of date a major version and might contains security issue. " . $vcve[1], $stackPtr, 'D7WarnAdvisoriesContribFoundMajor');
+							elseif ($diff <= 0) {
+								if (preg_match('/x$/', $vcve[0])) {
+									$phpcsFile->addError("Module " . $info['project'] ." ". $info['version'] . " contains security issue to all $vcve[0] versions. " . $vcve[1], $stackPtr, 'D7ErrAdvisoriesContribFoundMajor');
 								} else {
-									echo "MAJOR ERROR IN LOGIC!!!!!\n";
+									//echo "$fileName: SAFE! " . $info['version'] . "\n";
 								}
+							} else {
+								echo "MAJOR ERROR IN LOGIC!!!!!\n";
 							}
-					}
-				} else {
-					$phpcsFile->addWarning("Module " . $info['project'] . " is listed in advisories but file doesn't provide version information. Please use packages from drupal.org", $stackPtr, 'D7WarnAdvisoriesContribNoInfo');
+						}
 				}
+			} else {
+				$phpcsFile->addWarning("Module " . $info['project'] . " is listed in advisories but file doesn't provide version information. Please use packages from drupal.org", $stackPtr, 'D7WarnAdvisoriesContribNoInfo');
 			}
 		}
+
 	}
 
 }
