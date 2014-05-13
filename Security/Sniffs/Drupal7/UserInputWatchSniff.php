83b95b1b5681b75aebde09100f0122abace8acf3
Jonathan Marcil <jonathan.marcil@gmail.com>
2014-05-13 11:38:39 -0400

    Added Threshold for $form and $form_state possible user input with UserInputWatchSniff

--- Security/Sniffs/Drupal7/UserInputWatchSniff.php
+++ Security/Sniffs/Drupal7/UserInputWatchSniff.php
@@ -14,10 +14,29 @@ class Security_Sniffs_Drupal7_UserInputWatchSniff implements PHP_CodeSniffer_Sni
 
 	/**
 	* Paranoya mode. Will generate more alerts but will miss less vulnerabilites.
+	* This sniff depends on it to be set, otherwise it won't be excuted.
 	*
 	* @var bool
 	*/
 	public $ParanoiaMode = 1;
+
+	/**
+	* Threshold for $form and $form_state possible user input.
+	* Will generate one alert per file when this threshold is reached.
+	*
+	* @var int
+	* @var int
+	*/
+	public $FormThreshold =  10;
+	public $FormStateThreshold =  10;
+
+	/**
+	* Privates variables used for the above threshold
+	* @var int
+	* @var int
+	*/
+	private static $form_count = 0;
+	private static $form_state_count = 0;
 
 	/**
 	* Processes the tokens that this sniff is interested in.
@@ -33,9 +52,20 @@ class Security_Sniffs_Drupal7_UserInputWatchSniff implements PHP_CodeSniffer_Sni
 
 		$utils = new Security_Sniffs_Drupal7_Utils();
 		$tokens = $phpcsFile->getTokens();
-
 		if ($utils::is_token_user_input($tokens[$stackPtr])) {
-					$phpcsFile->addWarning('User input detetected with '.$tokens[$stackPtr]['content'].'.', $stackPtr, 'D7UserInWaWarn');
+			if ($tokens[$stackPtr]['content'] == '$form') {
+				// $form and $form_state are generating too much noise, let's do a count and make a generic warning for the file instead for each line
+				if (++self::$form_count == $this->FormThreshold) {
+					$phpcsFile->addWarning('At least '.$this->FormThreshold.' possible user input detetected with '.$tokens[$stackPtr]['content'].'.', $stackPtr, 'D7UserInWaFormWarn');
+				}
+			} elseif ($tokens[$stackPtr]['content'] == '$form_state') {
+				// $form and $form_state are generating too much noise, let's do a count and make a generic warning for the file instead for each line
+				if (++self::$form_state_count == $this->FormStateThreshold) {
+					$phpcsFile->addWarning('At least '.$this->FormStateThreshold.' possible user input detetected with '.$tokens[$stackPtr]['content'].'.', $stackPtr, 'D7UserInWaFormStateWarn');
+				}
+			} else {
+				$phpcsFile->addWarning('User input detetected with '.$tokens[$stackPtr]['content'].'.', $stackPtr, 'D7UserInWaWarn');
+			}
 		}
 	}
 
