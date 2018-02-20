2b2c72689a0cdc75afbb5ace7f52199d17d857c8
Jonathan <jonathan.marcil@gmail.com>
2018-02-19 19:27:23 -0800

    added namespace to be able to load Drupal7 utils

--- Security/Sniffs/UtilsFactory.php
+++ Security/Sniffs/UtilsFactory.php
@@ -5,7 +5,7 @@ class UtilsFactory {
 	public static function getInstance() {
 		$cmsframework = \PHP_CodeSniffer\Config::getConfigData('CmsFramework');
 		if (isset($cmsframework)) {
-			$utilsclass = 'Security_Sniffs_'.$cmsframework.'_Utils';
+			$utilsclass = '\\PHPCS_SecurityAudit\\Sniffs\\'.$cmsframework.'\\Utils';
 			if (class_exists($utilsclass)) {
 				return new $utilsclass();
 			} else {
