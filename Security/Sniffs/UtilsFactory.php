47d241a8c3b9fcb011c6d6fc5495dd1cc19728e0
jmarcil <jonathan.marcil@owasp.org>
2020-02-17 23:31:36 -0800

    fixes #58
    just had to add \\Security

--- Security/Sniffs/UtilsFactory.php
+++ Security/Sniffs/UtilsFactory.php
@@ -5,7 +5,7 @@ class UtilsFactory {
 	public static function getInstance() {
 		$cmsframework = \PHP_CodeSniffer\Config::getConfigData('CmsFramework');
 		if (isset($cmsframework)) {
-			$utilsclass = '\\PHPCS_SecurityAudit\\Sniffs\\'.$cmsframework.'\\Utils';
+			$utilsclass = '\\PHPCS_SecurityAudit\\Security\\Sniffs\\'.$cmsframework.'\\Utils';
 			if (class_exists($utilsclass)) {
 				return new $utilsclass();
 			} else {
